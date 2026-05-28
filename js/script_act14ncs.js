7ec11f9f4644589840cf5501e36fad362c618d81
mismomual-ui <mismomual@gmail.com>
2026-05-28 10:58:12 +0200

    Add files via upload
    Version Final

--- js/script_act14ncs.js
+++ js/script_act14ncs.js
@@ -3,6 +3,7 @@ let contatc = document.querySelector("#contacto");
 let info = document.querySelector("#info");
 let preview = document.querySelector("#preview");
 let bases = document.querySelector("#bases");
+let pie = document.querySelector("#pie");
 
 
 
@@ -17,12 +18,14 @@ function cambioVendido(e){
         info.classList.remove("collapse");
         preview.classList.add("collapse");
         bases.classList.add("collapse");
+        pie.classList.remove("fijo");
     }
     else if(e.target.getAttribute("data-posicion")=="3"){
         contatc.classList.remove("collapse");
         info.classList.add("collapse");
         preview.classList.add("collapse");
         bases.classList.add("collapse");
+        pie.classList.add("fijo");
         
     }
     else if(e.target.getAttribute("data-posicion")=="2"){
@@ -30,11 +33,13 @@ function cambioVendido(e){
         info.classList.add("collapse");
         preview.classList.remove("collapse");
         bases.classList.add("collapse");
+        pie.classList.remove("fijo");
     }
     else{
         contatc.classList.add("collapse");
         info.classList.add("collapse");
         preview.classList.add("collapse");
         bases.classList.remove("collapse");
+        pie.classList.add("fijo");
     }
 }
